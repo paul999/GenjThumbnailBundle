704b36ccfdfd71da79025b4e1e5bd7c3c1d0b2df
Maarten de Keizer <github@maartendekeizer.nl>
2017-01-21 22:31:40 +0100

    #13944 Code style fixes, add remove on CDN support, composer suggest  FlySystemBundle

--- src/Genj/ThumbnailBundle/Imagine/Cache/Resolver/LocalAndCdnResolver.php
+++ src/Genj/ThumbnailBundle/Imagine/Cache/Resolver/LocalAndCdnResolver.php
@@ -16,24 +16,24 @@ use Symfony\Component\Routing\RequestContext;
  */
 class LocalAndCdnResolver extends BaseResolver
 {
-	/**
-	 * If true interact with localstorage and CDN
-	 * @var boolean
-	 */
-	protected $useCdn = false;
-	
-	/**
-	 * @var \League\Flysystem\Filesystem
-	 */
-	protected $cdn;
-	
-	public function __construct(SfFilesystem $filesystem, RequestContext $requestContext, $webRootDir, $cachePrefix = 'media/cache', $useCdn = false, FlyFilesystem $cdn = null)
-	{
-		$this->useCdn = $useCdn;
-		$this->cdn = $cdn;
-		parent::__construct($filesystem, $requestContext, $webRootDir, $cachePrefix);
-	}
-	
+    /**
+     * If true interact with localstorage and CDN
+     * @var boolean
+     */
+    protected $useCdn = false;
+
+    /**
+     * @var \League\Flysystem\Filesystem
+     */
+    protected $cdn;
+
+    public function __construct(SfFilesystem $filesystem, RequestContext $requestContext, $webRootDir, $cachePrefix = 'media/cache', $useCdn = false, FlyFilesystem $cdn = null)
+    {
+        $this->useCdn = $useCdn;
+        $this->cdn = $cdn;
+        parent::__construct($filesystem, $requestContext, $webRootDir, $cachePrefix);
+    }
+
     /**
      * Override to return just the path itself. We determine our path using the routing, so there is no need to change
      * the path here.
@@ -47,35 +47,48 @@ class LocalAndCdnResolver extends BaseResolver
     {
         return $path;
     }
-    
+
+    /**
+     * {@inheritDoc}
+     * @see \Liip\ImagineBundle\Imagine\Cache\Resolver\WebPathResolver::store()
+     */
     public function store(BinaryInterface $binary, $path, $filter)
     {
-    	parent::store($binary, $path, $filter);
-    	
-    	if ($this->useCdn === true && $this->cdn !== null) {
-    		if ($this->cdn->has($path) === false) {
-    			// $this->getFilePath($path, $filter) instead of $path ??
-    			$this->cdn->put($path, $binary->getContent());
-    		}
-    	}
+        // run the file system store action
+        parent::store($binary, $path, $filter);
+
+        // put file on cdn
+        if ($this->useCdn === true && $this->cdn !== null) {
+            if ($this->cdn->has($path) === false) {
+                $this->cdn->put($path, $binary->getContent());
+            }
+        }
     }
-    
+
+    /**
+     * {@inheritDoc}
+     * @see \Liip\ImagineBundle\Imagine\Cache\Resolver\WebPathResolver::remove()
+     */
     public function remove(array $paths, array $filters)
     {
-    	parent::remove($paths, $filters);
-    
-    	if ($this->useCdn === true && $this->cdn !== null) {
-    		if (empty($paths)) {
-    			foreach ($filters as $filter) {
-    				$this->cdn->deleteDir($filter);
-    			}
-    			return;
-    		}
-    		foreach ($paths as $path) {
-    			foreach ($filters as $filter) {
-    				$this->cdn->delete($this->getFilePath($path, $filter));
-    			}
-    		}
-    	}
+        // run the file system remove action
+        parent::remove($paths, $filters);
+
+        // remove files on cdn
+        if ($this->useCdn === true && $this->cdn !== null) {
+           if (empty($paths)) {
+               foreach ($filters as $filter) {
+                   $this->cdn->deleteDir($filter);
+                }
+                return;
+            }
+            foreach ($paths as $path) {
+                foreach ($filters as $filter) {
+                    if ($this->cdn->has($path)) {
+                        $this->cdn->delete($path);
+                    }
+                }
+            }
+        }
     }
 }
